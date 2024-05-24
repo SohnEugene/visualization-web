d4827deb16ae1bd683e2524d185d8d7005b25c71
SohnEugene <qeugene2328@gmail.com>
2024-05-24 21:51:52 +0900

    check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Section title="이론, 가설 소개" className="section-intro" content={generateLongContent("이론, 가설 소개", 10)} />
+      <Section title="이론 소개" className="section-intro" content={generateLongContent("이론, 가설 소개", 10)} />
       <Section title="스펙트럼" className="section-data1" content={generateLongContent("스펙트럼", 100)} />
       <Section title="지역별" className="section-data2" content={generateLongContent("지역별", 100)} />
       <Section title="결론" className="section-conclusion" content={generateLongContent("결론", 20)} />
